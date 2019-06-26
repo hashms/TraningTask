1a8573b1b3840f7f94d7ed9149d305ede7f4a766
m <m@local>
2019-06-26 13:37:30 +0900

    add 2files

--- MultiConvertRfc.cs
+++ MultiConvertRfc.cs
@@ -8,10 +8,6 @@ public class MultiConvert
 
         if (args[0] == "10to2" )
         {
-            //num = int.Parse(args[1]);
-            //num = int.Parse(Convert.ToString(num, 2));
-            //Console.WriteLine($"2進数：{num}" );
-
             Console.WriteLine($"16進数：{Convert.ToString(int.Parse(args[1]), 2)}");
         }
         else if (args[0] == "10to16")
